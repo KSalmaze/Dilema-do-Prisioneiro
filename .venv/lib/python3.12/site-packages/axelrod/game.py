e52d2613663894a23f44db5eea4339435cb62d61
pwdrc <tolvo.pedro@gmail.com>
2024-06-22 15:07:22 -0300

    cria requirements.txt

---
+++ .venv/lib/python3.12/site-packages/axelrod/game.py
@@ -0,0 +1,135 @@
+from typing import Tuple, Union
+from enum import Enum
+
+import numpy as np
+
+from axelrod import Action
+
+C, D = Action.C, Action.D
+
+Score = Union[int, float]
+
+
+class AsymmetricGame(object):
+    """Container for the game matrix and scoring logic.
+
+    Attributes
+    ----------
+    scores: dict
+        The numerical score attribute to all combinations of action pairs.
+    """
+
+    # pylint: disable=invalid-name
+    def __init__(self, A: np.array, B: np.array) -> None:
+        """
+        Creates an asymmetric game from two matrices.
+
+        Parameters
+        ----------
+        A: np.array
+            the payoff matrix for player A.
+        B: np.array
+            the payoff matrix for player B.
+        """
+
+        if A.shape != B.transpose().shape:
+            raise ValueError(
+                "AsymmetricGame was given invalid payoff matrices; the shape "
+                "of matrix A should be the shape of B's transpose matrix."
+            )
+
+        self.A = A
+        self.B = B
+
+        self.scores = {
+            pair: self.score(pair) for pair in ((C, C), (D, D), (C, D), (D, C))
+        }
+
+    def score(
+        self, pair: Union[Tuple[Action, Action], Tuple[int, int]]
+    ) -> Tuple[Score, Score]:
+        """Returns the appropriate score for a decision pair.
+        Parameters
+        ----------
+        pair: tuple(int, int) or tuple(Action, Action)
+            A pair of actions for two players, for example (0, 1) corresponds
+            to the row player choosing their first action and the column
+            player choosing their second action; in the prisoners' dilemma,
+            this is equivalent to player 1 cooperating and player 2 defecting.
+            Can also be a pair of Actions, where C corresponds to '0'
+            and D to '1'.
+
+        Returns
+        -------
+        tuple of int or float
+            Scores for two player resulting from their actions.
+        """
+
+        # if an Action has been passed to the method,
+        # get which integer the Action corresponds to
+        def get_value(x):
+            if isinstance(x, Enum):
+                return x.value
+            return x
+        row, col = map(get_value, pair)
+
+        return (self.A[row][col], self.B[row][col])
+
+    def __repr__(self) -> str:
+        return "Axelrod game with matrices: {}".format((self.A, self.B))
+
+    def __eq__(self, other):
+        if not isinstance(other, AsymmetricGame):
+            return False
+        return self.A.all() == other.A.all() and self.B.all() == other.B.all()
+
+
+class Game(AsymmetricGame):
+    """
+    Simplification of the AsymmetricGame class for symmetric games.
+    Takes advantage of Press and Dyson notation.
+
+    Can currently only be 2x2.
+
+    Attributes
+    ----------
+    scores: dict
+        The numerical score attribute to all combinations of action pairs.
+    """
+
+    def __init__(self, r: Score = 3, s: Score = 0, t: Score = 5, p: Score = 1) -> None:
+        """Create a new game object.
+
+        Parameters
+        ----------
+        r: int or float
+            Score obtained by both players for mutual cooperation.
+        s: int or float
+            Score obtained by a player for cooperating against a defector.
+        t: int or float
+            Score obtained by a player for defecting against a cooperator.
+        p: int or float
+            Score obtained by both player for mutual defection.
+        """
+        A = np.array([[r, s], [t, p]])
+
+        super().__init__(A, A.transpose())
+
+    def RPST(self) -> Tuple[Score, Score, Score, Score]:
+        """Returns game matrix values in Press and Dyson notation."""
+        R = self.scores[(C, C)][0]
+        P = self.scores[(D, D)][0]
+        S = self.scores[(C, D)][0]
+        T = self.scores[(D, C)][0]
+        return R, P, S, T
+
+    def __repr__(self) -> str:
+        return "Axelrod game: (R,P,S,T) = {}".format(self.RPST())
+
+    def __eq__(self, other):
+        if not isinstance(other, Game):
+            return False
+        return self.RPST() == other.RPST()
+
+
+DefaultGame = Game()
---
+++ .venv/lib/python3.12/site-packages/axelrod/tests/unit/test_game.py
@@ -0,0 +1,130 @@
+import unittest
+
+import numpy as np
+
+import axelrod as axl
+from axelrod.tests.property import games, asymmetric_games
+from hypothesis import given, settings
+from hypothesis.strategies import integers
+from hypothesis.extra.numpy import arrays, array_shapes
+
+
+C, D = axl.Action.C, axl.Action.D
+
+
+class TestGame(unittest.TestCase):
+    def test_default_scores(self):
+        expected_scores = {
+            (C, D): (0, 5),
+            (D, C): (5, 0),
+            (D, D): (1, 1),
+            (C, C): (3, 3),
+        }
+        self.assertEqual(axl.Game().scores, expected_scores)
+
+    def test_default_RPST(self):
+        expected_values = (3, 1, 0, 5)
+        self.assertEqual(axl.Game().RPST(), expected_values)
+
+    def test_default_score(self):
+        game = axl.Game()
+        self.assertEqual(game.score((C, C)), (3, 3))
+        self.assertEqual(game.score((D, D)), (1, 1))
+        self.assertEqual(game.score((C, D)), (0, 5))
+        self.assertEqual(game.score((D, C)), (5, 0))
+
+    def test_default_equality(self):
+        self.assertEqual(axl.Game(), axl.Game())
+
+    def test_not_default_equality(self):
+        self.assertEqual(axl.Game(1, 2, 3, 4), axl.Game(1, 2, 3, 4))
+        self.assertNotEqual(axl.Game(1, 2, 3, 4), axl.Game(1, 2, 3, 5))
+        self.assertNotEqual(axl.Game(1, 2, 3, 4), axl.Game())
+
+    def test_wrong_class_equality(self):
+        self.assertNotEqual(axl.Game(), "wrong class")
+
+    @given(r=integers(), p=integers(), s=integers(), t=integers())
+    @settings(max_examples=5)
+    def test_random_init(self, r, p, s, t):
+        """Test init with random scores using the hypothesis library."""
+        expected_scores = {
+            (C, D): (s, t),
+            (D, C): (t, s),
+            (D, D): (p, p),
+            (C, C): (r, r),
+        }
+        game = axl.Game(r, s, t, p)
+        self.assertEqual(game.scores, expected_scores)
+
+    @given(r=integers(), p=integers(), s=integers(), t=integers())
+    @settings(max_examples=5)
+    def test_random_RPST(self, r, p, s, t):
+        """Test RPST method with random scores using the hypothesis library."""
+        game = axl.Game(r, s, t, p)
+        self.assertEqual(game.RPST(), (r, p, s, t))
+
+    @given(r=integers(), p=integers(), s=integers(), t=integers())
+    @settings(max_examples=5)
+    def test_random_score(self, r, p, s, t):
+        """Test score method with random scores using the hypothesis library."""
+        game = axl.Game(r, s, t, p)
+        self.assertEqual(game.score((C, C)), (r, r))
+        self.assertEqual(game.score((D, D)), (p, p))
+        self.assertEqual(game.score((C, D)), (s, t))
+        self.assertEqual(game.score((D, C)), (t, s))
+
+    @given(game=games())
+    @settings(max_examples=5)
+    def test_random_repr(self, game):
+        """Test repr with random scores using the hypothesis library."""
+        expected_repr = "Axelrod game: (R,P,S,T) = {}".format(game.RPST())
+        self.assertEqual(expected_repr, game.__repr__())
+        self.assertEqual(expected_repr, str(game))
+
+    @given(game=games())
+    def test_integer_actions(self, game):
+        """Test Actions and integers are treated equivalently."""
+        pair_ints = {
+            (C, C): (0 ,0),
+            (C, D): (0, 1),
+            (D, C): (1, 0),
+            (D, D): (1, 1)
+        }
+        for key, value in pair_ints.items():
+            self.assertEqual(game.score(key), game.score(value))
+
+class TestAsymmetricGame(unittest.TestCase):
+    @given(A=arrays(int, array_shapes(min_dims=2, max_dims=2, min_side=2)),
+           B=arrays(int, array_shapes(min_dims=2, max_dims=2, min_side=2)))
+    @settings(max_examples=5)
+    def test_invalid_matrices(self, A, B):
+        """Test that an error is raised when the matrices aren't the right size."""
+        # ensures that an error is raised when the shapes are invalid,
+        # and not raised otherwise
+        error_raised = False
+        try:
+            game = axl.AsymmetricGame(A, B)
+        except ValueError:
+            error_raised = True
+
+        self.assertEqual(error_raised, (A.shape != B.transpose().shape))
+
+    @given(asymgame=asymmetric_games())
+    @settings(max_examples=5)
+    def test_random_repr(self, asymgame):
+        """Test repr with random scores."""
+        expected_repr = "Axelrod game with matrices: {}".format((asymgame.A, asymgame.B))
+        self.assertEqual(expected_repr, asymgame.__repr__())
+        self.assertEqual(expected_repr, str(asymgame))
+
+    @given(asymgame1=asymmetric_games(),
+           asymgame2=asymmetric_games())
+    @settings(max_examples=5)
+    def test_equality(self, asymgame1, asymgame2):
+        """Tests equality of AsymmetricGames based on their matrices."""
+        self.assertFalse(asymgame1=='foo')
+        self.assertEqual(asymgame1, asymgame1)
+        self.assertEqual(asymgame2, asymgame2)
+        self.assertEqual((asymgame1==asymgame2), (asymgame1.A.all() == asymgame2.A.all()
+                                                  and asymgame1.B.all() == asymgame2.B.all()))
