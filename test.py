d6350daec398223f5492c711506d16e172e0a25b
pedro <98365041+pwdrc@users.noreply.github.com>
2024-06-23 18:45:33 +0000

    salvar em csv (rever logico rdps)

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import axelrod as axl
 import json
+import csv
 
-# Definir as estratégias corrigidas
+# Definir as estratégias
 strategies = [
     axl.Cooperator(),
     axl.Defector(),
@@ -26,13 +27,36 @@ noise_levels = [0.0, 0.1, 0.2, 0.3, 0.4]
 # Dicionário para armazenar os resultados
 tournament_results = {}
 
-# Gerar campeonatos
+# Gerar campeonatos (rever a logica dps)
 for noise in noise_levels:
     tournament = axl.Tournament(strategies, repetitions=5, noise=noise)
     results = tournament.play()
     # Armazenar resultados
-    tournament_results[f'Noise_{noise}'] = results.summarise()
+    summary = results.summarise()
+    scores_sum = {}
+    for entry in summary:
+        strategy_name = entry[1]  # Ajuste para acessar o nome da estratégia por índice
+        if strategy_name not in scores_sum:
+            print(strategy_name)
+            scores_sum[strategy_name] = 0
+        scores_sum[strategy_name] += entry[2]
+
+        tournament_results[f'Noise_{noise}'] = scores_sum
+    
+    for strategy in strategies:
+        if strategy.name not in scores_sum:
+            scores_sum[strategy.name] = 0
 
 # Salvar resultados em um arquivo JSON
-with open('tournament_results.json', 'w') as f:
-    json.dump(tournament_results, f)
+# with open('tournament_results.json', 'w') as f:
+#     json.dump(tournament_results, f)
+
+# Salvar em csv
+with open('tournament_results.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Strategy'] + list(tournament_results.keys()))
+    for strategy in strategies:
+        row = [strategy.name]
+        for noise in noise_levels:
+            row.append(tournament_results[f'Noise_{noise}'][strategy.name])
+        writer.writerow(row)
